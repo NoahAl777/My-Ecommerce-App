fc2e14412a30251c910e06c77590cc9dea6d1d65
noah <crashlive112@gmail.com>
2021-05-14 23:01:12 -0500

    add Users controller and Sign Up form

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,11 @@
+class UsersController < ApplicationController
+
+  # GET /signup - New User Action
+  def new #actions
+
+  end
+
+  # POST /users - Create the User Action
+  def create
+  end
+end
